87b753016ddcfc6e2677e8246e6ca09d04082c05
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-06-08 01:06:52 +0300

    Improved speed - now loading courses concurrently!

--- src/main/java/ru/abzaltdinov/stepiktopcourses/service/CoursesLoaderService.java
+++ src/main/java/ru/abzaltdinov/stepiktopcourses/service/CoursesLoaderService.java
@@ -1,29 +1,33 @@
 package ru.abzaltdinov.stepiktopcourses.service;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import ru.abzaltdinov.stepiktopcourses.client.StepikAPIClient;
 import ru.abzaltdinov.stepiktopcourses.model.Course;
 import ru.abzaltdinov.stepiktopcourses.model.CoursesWithMeta;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 @Service
 public class CoursesLoaderService {
 
     private final StepikAPIClient stepikAPIClient;
+    private final Logger logger = LoggerFactory.getLogger(CoursesLoaderService.class);
 
-    private Set<Course> allCourses = new HashSet<>();
-    private ArrayList<Course> sortedCourses = new ArrayList<>();
     private Comparator<Course> courseComparator =
             Comparator.comparingLong(Course::getLearnersCount).reversed();
+
+    private TreeSet<Course> allSortedCourses = new TreeSet<>(courseComparator);
+
 
     @Autowired
     public CoursesLoaderService(StepikAPIClient stepikAPIClient) {
@@ -34,53 +38,79 @@ public class CoursesLoaderService {
         if (amount <= 0) {
             return null;
         }
+
         List<Course> newCourses = loadNewCourses();
-        if (allCourses.isEmpty()) {
-            sortedCourses.addAll(newCourses);
-            sortedCourses.sort(courseComparator);
-        } else {
-            for (Course course : newCourses) {
-                int indexToInsert = Collections.binarySearch(sortedCourses, course, courseComparator);
-                if (indexToInsert < 0) {
-                    indexToInsert = -indexToInsert - 1;
-                }
-                sortedCourses.add(indexToInsert, course);
+
+        if (newCourses == null) {
+            return null;
+        }
+        allSortedCourses.addAll(newCourses);
+        if (amount > allSortedCourses.size()) {
+            amount = allSortedCourses.size();
+        }
+        ArrayList<Course> resultedTopCourses = new ArrayList<>(amount);
+        int currAmount = 0;
+        for (Course course : allSortedCourses) {
+            resultedTopCourses.add(course);
+            currAmount++;
+            if (currAmount == amount) {
+                break;
             }
         }
-        allCourses.addAll(newCourses);
-        if (amount > sortedCourses.size()) {
-            amount = sortedCourses.size();
-        }
-        return sortedCourses.subList(0, amount);
+        return resultedTopCourses;
     }
 
     private List<Course> loadNewCourses() {
-        List<Course> result = new ArrayList<>();
-        int nextPage = 1;
-        boolean hasNextPage = true;
-        boolean loadedAlreadyExistedCourses = false;
-
-        while (hasNextPage && !loadedAlreadyExistedCourses) {
-            Response<CoursesWithMeta> coursesWithMetaResponse = null;
-            try {
-                coursesWithMetaResponse = stepikAPIClient.getCourses(nextPage).execute();
-            } catch (IOException e) {
-                break;
-            }
-            if (!coursesWithMetaResponse.isSuccessful()) {
-                break;
-            }
-            CoursesWithMeta coursesWithMeta = coursesWithMetaResponse.body();
-            for (Course course : coursesWithMeta.getCourses()) {
-                if (allCourses.contains(course)) {
-                    loadedAlreadyExistedCourses = true;
-                    break;
+        int coresAmount = Runtime.getRuntime().availableProcessors();
+        List<Callable<List<Course>>> loadCoursesTasks = new ArrayList<>(coresAmount);
+        for (int i = 0; i < coresAmount; ++i) {
+            final int threadNumber = i + 1;
+            loadCoursesTasks.add(() -> {
+                List<Course> result = new ArrayList<>();
+                int pageNumber = threadNumber;
+                boolean loadedAlreadyExistingCourses = false;
+                while (!loadedAlreadyExistingCourses) {
+                    Response<CoursesWithMeta> coursesWithMetaResponse;
+                    try {
+                        coursesWithMetaResponse = stepikAPIClient.getCourses(pageNumber).execute();
+                    } catch (IOException e) {
+                        break;
+                    }
+                    if (!coursesWithMetaResponse.isSuccessful() || coursesWithMetaResponse.body() == null) {
+                        break;
+                    }
+                    logger.debug("PAGE " + pageNumber + " of Stepik API loaded.");
+                    CoursesWithMeta coursesWithMeta = coursesWithMetaResponse.body();
+                    for (Course course : coursesWithMeta.getCourses()) {
+                        if (allSortedCourses.contains(course)) {
+                            loadedAlreadyExistingCourses = true;
+                            break;
+                        }
+                        result.add(course);
+                    }
+                    pageNumber += coresAmount;
                 }
-                result.add(course);
-            }
-            hasNextPage = coursesWithMeta.getMeta().isHasNext();
-            nextPage++;
+                return result;
+            });
         }
-        return result;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(coresAmount);
+        List<Course> newLoadedCourses = null;
+        try {
+            newLoadedCourses = executorService.invokeAll(loadCoursesTasks)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception e) {
+                            throw new IllegalStateException(e);
+                        }
+                    })
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
+        } catch (InterruptedException e) {
+            logger.debug(e.toString());
+        }
+        return newLoadedCourses;
     }
 }
